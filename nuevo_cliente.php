23da2db1c87a806fdffd16a64b071198fd1a1604
Diego Cornalo <diegocornalo95@gmail.com>
2016-09-04 18:51:21 -0300

    Actualizado archivo clientes

--- nuevo_cliente.php
+++ nuevo_cliente.php
@@ -11,9 +11,14 @@ if ($_POST) {
     // Guardamos valores de inputs con escape de caracteres raros y/o sospechosos.
     $cli_apellido = $bd->real_escape_string($_POST["cli_apellido"]);
     $cli_nombre = $bd->real_escape_string($_POST["cli_nombre"]);
-    $alu_fecha_alta = date("Y-m-d");
+    $cli_dni = $bd->real_escape_string($_POST["cli_dni"]);
+    $cli_direccion = $bd->real_escape_string($_POST["cli_direccion"]);
+    $cli_telefono = $bd->real_escape_string($_POST["cli_telefono"]);
+     $cli_id_localidad = $bd->real_escape_string($_POST["cli_id_localidad"]);
+      $cli_email = $bd->real_escape_string($_POST["cli_email"]);
+    //$alu_fecha_alta = date("Y-m-d");
 
-    $sql_alu = "INSERT INTO clientes (
+    $sql_cli = "INSERT INTO clientes (
                     cli_apellido,
                     cli_nombre,
                     cli_dni,
@@ -31,9 +36,11 @@ if ($_POST) {
                     '{$cli_id_localidad}',
                     '{$cli_email}',
                     '{$cli_estado}')";
+/*echo "<pre>$sql_cli</pre>";
+exit;*/
 
-    $insert_alu = $bd->query($sql_alu);
-    if ( ! $insert_alu) {
+    $insert_cli = $bd->query($sql_cli);
+    if ( ! $insert_cli) {
         $msg = "Ocurrió un error al intentar guardar: " . $bd->error;
     } else {
         $msg = "El registro fué completado con éxito.";
@@ -44,7 +51,7 @@ if ($_POST) {
     // navegador pueda preguntar si volver a enviar el formulario.
     echo "<script>
             alert('" . addslashes($msg) . "');
-            location.href = 'nuevo_alumno.php';
+            location.href = 'nuevo_cliente.php';
         </script>";
 
     // Una vez que hicimos "echo" de la redirección con JS, detenemos
@@ -64,27 +71,23 @@ include "parte-superior.php";
 $sql_tdoc = "SELECT tdoc_id, tdoc_nombre FROM tipo_documentos";
 $query_tdoc = $bd->query($sql_tdoc);
 
-// Paises
-$sql_pais = "SELECT pais_id, pais_nombre FROM paises";
-$query_pais = $bd->query($sql_pais);
-
-// Provincias
-$sql_prov = "SELECT prov_id, prov_nombre FROM provincias";
-$query_prov = $bd->query($sql_prov);
+// Localidades
+$sql_loc = "SELECT loc_id, loc_nombre FROM localidades";
+$query_loc = $bd->query($sql_loc);
 ?>
 
 <div id="contenido">
-    <form id="" action="nuevo_alumno.php" method="post">
-        <h2>Nuevo Alumno</h2>
+    <form id="" action="nuevo_cliente.php" method="post">
+        <h2>Nuevo Cliente</h2>
 
-        <label for="alu_apellido">Apellido:</label>
-        <input name="alu_apellido" id="alu_apellido" type="text">
+        <label for="cli_apellido">Apellido:</label>
+        <input name="cli_apellido" id="cli_apellido" type="text">
         <br><br>
-        <label for="alu_nombre">Nombre:</label>
-        <input name="alu_nombre" id="alu_nombre" type="text">
+        <label for="cli_nombre">Nombre:</label>
+        <input name="cli_nombre" id="cli_nombre" type="text">
         <br><br>
-        <label for="alu_id_tipo_doc">Tipo documento:</label>
-        <select name="alu_id_tipo_doc">
+        <!--<label for="cli_id_tipo_doc">Tipo documento:</label>
+        <select name="cli_id_tipo_doc">
             <option value="">elegir...</option>
             <?php while($tdoc = $query_tdoc->fetch_object()): ?>
                 <option value="<?= $tdoc->tdoc_id ?>">
@@ -92,19 +95,28 @@ $query_prov = $bd->query($sql_prov);
                 </option>
             <?php endwhile; ?>
         </select>
+        <br><br>-->
+        <label for="cli_dni">Doc./clave:</label>
+        <input name="cli_dni" id="cli_dni" type="number">
         <br><br>
-        <label for="alu_numero_doc">Doc./clave:</label>
-        <input name="alu_numero_doc" id="alu_numero_doc" type="text">
+         <label for="cli_direccion">Direccion:</label>
+        <input name="cli_direccion" id="cli_direccion" type="text">
         <br><br>
-        <label for="alu_id_provincia">Provincia:</label>
-        <select name="alu_id_provincia">
+         <label for="cli_telefono">N° Telefono:</label>
+        <input name="cli_telefono" id="cli_telefono" type="text">
+        <br><br>
+        <label for="cli_id_localidad">Localidad:</label>
+        <select name="cli_id_localidad">
             <option value="">elegir...</option>
-            <?php while($prov = $query_prov->fetch_object()): ?>
-                <option value="<?= $prov->prov_id ?>">
-                    <?= $prov->prov_nombre ?>
+            <?php while($loc = $query_loc->fetch_object()): ?>
+                <option value="<?= $loc->loc_id ?>">
+                    <?= $loc->loc_nombre ?>
                 </option>
             <?php endwhile; ?>
         </select>
+        <br><br>
+         <label for="cli_email">Email:</label>
+        <input name="cli_email" id="cli_email" type="email">
         <br><br>
         <input type="submit" name="formulario" value="Enviar">
     </form>
